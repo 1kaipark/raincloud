90b3f4c1d10f3dfb08f5f36b859522ead7423358
Kai Park <141146159+1kaipark@users.noreply.github.com>
2024-02-28 22:10:49 -0500

    Update raincloud.py
    playlist support

--- raincloud.py
+++ raincloud.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.raincloud import Downloader
+from lib.Downloader import Downloader
 import os
 
 client_id_filepath = os.path.join('lib', 'client_id.txt')
@@ -8,7 +8,7 @@ with open(client_id_filepath, 'r') as client_id_txt:
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "simple soundcloud downloader")
-	parser.add_argument('track_url', type = str, help = "soundcloud URL")
+	parser.add_argument('sc_url', type = str, help = "soundcloud URL")
 	parser.add_argument('--cid', type = str, default = client_id, help = "soundcloud client ID, can be obtained via F12 on refresh.")
 	parser.add_argument('--nm', default = False, action = 'store_true', help = 'just download mp3, no metadata')
 	parser.add_argument('--a', default = False, action = 'store_true', help = 'include artist (SC username) in file export')
@@ -16,11 +16,21 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	downloader = Downloader(args.cid)
-	res = downloader.get_resolved(args.track_url)
-	stream_url = downloader.get_streaming_url(res)
-	filename = f"{res.user.username} - {res.title}" if args.a else res.title
-	_, filepath = downloader.stream_download(stream_url, filename)
 
-	if args.nm == False:
-		title, artist = downloader.add_metadata(res, filepath)
-		print(f"metadata: {title}, {artist}.")
+	res_0 = downloader.get_resolved(args.sc_url)
+
+	urls = []
+	if res_0.kind == 'track':
+	    urls.append(res_0.permalink_url)
+	else:
+	    urls = downloader.playlist_to_tracks(res_0)
+
+	for track_url in urls:
+		res = downloader.get_resolved(track_url)
+		stream_url = downloader.get_streaming_url(res)
+		filename = f"{res.user.username} - {res.title}" if args.a else res.title
+		_, filepath = downloader.stream_download(stream_url, filename)
+
+		if args.nm == False:
+			title, artist = downloader.add_metadata(res, filepath)
+			print(f"metadata: {title}, {artist}.")
